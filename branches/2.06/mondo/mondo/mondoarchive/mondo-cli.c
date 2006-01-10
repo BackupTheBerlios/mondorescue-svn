93532c7e030e19ad75c6d54b764c011ebd958170
bcornec <bcornec@9760f4c4-1700-0410-9533-feba0de264b8>
2006-01-10 08:35:09 +0000

    Abort when one of the include dirs (-I) does not exist (bug reported by jeff@jlsworld.com) A warning is also logged for the -E option

--- branches/2.06/mondo/mondo/mondoarchive/mondo-cli.c
+++ branches/2.06/mondo/mondo/mondoarchive/mondo-cli.c
@@ -359,6 +359,8 @@ process_switches(struct s_bkpinfo *bkpinfo,
 
 	long itbs;
 
+	struct stat buf;
+
 	malloc_string(tmp);
 	malloc_string(psz);
 
@@ -438,6 +440,10 @@ process_switches(struct s_bkpinfo *bkpinfo,
 		}
 		if (bkpinfo->include_paths[0]) {
 			strcat(bkpinfo->include_paths, " ");
+		}
+		if (stat(flag_val['I'], &buf) != 0) {
+			log_msg("ERROR ! %s doesn't exist", flag_val['I']);
+			fatal_error("ERROR ! You specified a directory to include which doesn't exist");
 		}
 		strncpy(bkpinfo->include_paths + strlen(bkpinfo->include_paths),
 				flag_val['I'],
@@ -651,6 +657,9 @@ process_switches(struct s_bkpinfo *bkpinfo,
 	if (flag_set['E']) {
 		if (bkpinfo->exclude_paths[0]) {
 			strcat(bkpinfo->exclude_paths, " ");
+		}
+		if (stat(flag_val['E'], &buf) != 0) {
+			log_msg(1, "WARNING ! %s doesn't exist", flag_val['E']);
 		}
 		strncpy(bkpinfo->exclude_paths + strlen(bkpinfo->exclude_paths),
 				flag_val['E'],

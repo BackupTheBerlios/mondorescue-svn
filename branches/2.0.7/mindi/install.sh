1a72ecc2a55cc05e6ad29de0c3a79d9a33e0b6f7
bcornec <bcornec@9760f4c4-1700-0410-9533-feba0de264b8>
2006-04-02 22:10:37 +0000

    typo

--- branches/2.0.7/mindi/install.sh
+++ branches/2.0.7/mindi/install.sh
@@ -109,7 +109,7 @@ if [ "$RPMBUILDMINDI" != "true" ]; then
 fi
 
 # Special case for SuSE family wher doc is put elsewhere in the RPM
-if [ _"$dfam" != _"suse" ] then;
+if [ _"$dfam" != _"suse" ]; then
 	rm -rf $DOCDIR
 fi
 
